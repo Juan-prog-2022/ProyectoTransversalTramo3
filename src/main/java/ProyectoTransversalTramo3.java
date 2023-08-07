6160ea385a918b56f7ecb7e8ba29b31079065538
Juan Carlos Quiroz <quirozjuan497@gmail.com>
2023-08-07 11:24:11 -0300

    Initial commit

---
+++ src/main/java/ProyectoTransversalTramo3.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Juan
+ */
+public class ProyectoTransversalTramo3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
